2579b41f4128fcd2edfae391f7426a845ef91b00
Aitzhan <107255025+a1tzhan@users.noreply.github.com>
2025-04-02 14:35:25 +0500

    Update mux_tb.cpp

--- bitty_lab12/test/mux/mux_tb.cpp
+++ bitty_lab12/test/mux/mux_tb.cpp
@@ -1,58 +1,53 @@
 #include "Vmux.h"
 #include "verilated.h"
 #include <iostream>
+#include <cstdlib>
+#include <random>
+#include <bitset>
+#include <ctime>
 
 int main(int argc, char **argv)
 {
     Verilated::commandArgs(argc, argv);
     Vmux *tb = new Vmux;
 
-    uint16_t out = 0;
-    for (uint8_t sel = 0; sel < 8; sel++)
+    std::mt19937 gen(42); // Seed for reproducibility
+    std::uniform_int_distribution<uint32_t> dist(0, 0xFFFF);
+    std::bernoulli_distribution bdist(0.5);
+    srand(time(0));
+
+    uint16_t regs[8];
+
+    uint16_t out;
+    int errors = 0;
+    const int NUM_TESTS = 1000;
+
+    for (int j = 0; j < NUM_TESTS; j++)
     {
+        for (int i = 0; i < 8; i++)
+        {
+            regs[i] = dist(gen);
+        }
+        tb->reg_0 = regs[0];
+        tb->reg_1 = regs[1];
+        tb->reg_2 = regs[2];
+        tb->reg_3 = regs[3];
+        tb->reg_4 = regs[4];
+        tb->reg_5 = regs[5];
+        tb->reg_6 = regs[6];
+        tb->reg_7 = regs[7];
+        uint8_t sel = rand() % 8;
         tb->sel = sel;
         tb->eval();
-        switch (sel)
+        if (regs[sel] != tb->out)
         {
-        case 0:
-            if (out != tb->out)
-                std::cout << "Error Case 0";
-            break;
-        case 1:
-            if (out != tb->out)
-                std::cout << "Error Case 1";
-            break;
-        case 2:
-            if (out != tb->out)
-                std::cout << "Error Case 2";
-            break;
-        case 3:
-            if (out != tb->out)
-                std::cout << "Error Case 3";
-            break;
-        case 4:
-            if (out != tb->out)
-                std::cout << "Error Case 4";
-            break;
-        case 5:
-            if (out != tb->out)
-                std::cout << "Error Case 5";
-            break;
-        case 6:
-            if (out != tb->out)
-                std::cout << "Error Case 6";
-            break;
-        case 7:
-            if (out != tb->out)
-                std::cout << "Error Case 7";
-            break;
-        default:
-            break;
+            errors++;
+            std::cout << "Error, expected 0x" << std::hex << regs[sel] << ", but got 0x" << tb->out << std::dec << std::endl;
         }
     }
 
-    std::cout << " No errors found in Mux Tests\n";
+    std::cout << "\nMux tests complete. Errors: " << errors << std::endl;
     tb->final();
     delete tb;
-    return 0;
+    return errors ? 1 : 0;
 }
